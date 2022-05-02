43f24aed3ed94989df6be3c015728a56ea9366f7
echatziosmanاحمد احمد <vesselam_623@yahoo.gr>
2022-05-02 17:38:50 +0300

    2 May

---
+++ src/layouts/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navi from './Navi';
+import Categories from './Categories';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Navi />
+      <Categories></Categories>
+    </div>
+  );
+}
